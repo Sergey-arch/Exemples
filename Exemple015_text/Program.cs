5b2f19af260d7d641c47f24e0bd1bf67bad55669
sergey <mainsackliem@mail.ru>
2022-02-19 23:10:28 +0500

    text

---
+++ Exemple015_text/Program.cs
@@ -0,0 +1,33 @@
+﻿// Дан какой то текст
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "q w e r t y"
+//             0 1 2 3 4 5 
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty; // Пустая строка
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine(); // пустая строка для разделения
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
