1c663a6c2d81fd8f8832fe517cb1b2b01cbca059
sergey <mainsackliem@mail.ru>
2022-02-26 23:06:41 +0500

    017cb_Factorial_Double

---
+++ Exemple017cb_Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿// Факториал Решение с помощью рекурсии
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return (n * Factorial(n-1));
+}
+for (int i = 1; i < 41; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
+
